9503dd5cdf1f9bae82a99d5dfa7930f5ffc14af8
Alan Ficagna <alan.ficagna@gmail.com>
2018-12-10 16:06:50 -0200

    Redesign page layout.

--- lib/app_web/controllers/page_controller.ex
+++ lib/app_web/controllers/page_controller.ex
@@ -2,6 +2,8 @@ defmodule AppWeb.PageController do
   use AppWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    with {:ok, marvel_data} <- Marvel.get_story() do
+      render(conn, "index.html", marvel_data)
+    end
   end
 end
